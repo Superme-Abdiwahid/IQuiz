911cf1b26858a800e6f495c4c98835b5b7d6b635
Superme-Abdiwahid <abdiwahidhajir91@gmail.com>
2022-05-17 10:54:12 -0700

    Initial Commit

---
+++ IQUIZ_ATTEMP2UITests/IQUIZ_ATTEMP2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IQUIZ_ATTEMP2UITestsLaunchTests.swift
+//  IQUIZ_ATTEMP2UITests
+//
+//  Created by stlp on 5/17/22.
+//
+
+import XCTest
+
+class IQUIZ_ATTEMP2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
